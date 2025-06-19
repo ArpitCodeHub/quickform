76fb10d57f0e73e94000b6d6dd22bae18168cd38
ARPIT JAIN <ajarpit0705@gmail.com>
2025-06-19 09:28:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/resume-form/form-helpers.tsx
+++ src/components/resume-form/form-helpers.tsx
@@ -1,5 +1,6 @@
+
 "use client";
-import type React from 'react';
+import * as React from 'react';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
